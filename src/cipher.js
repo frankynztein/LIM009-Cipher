3c5682e5224dd8aaccb6fcac1a0e294d16be29c1
frankynztein <arellanfranciree@gmail.com>
2019-02-21 19:41:38 -0500

    Función encode para más de un caracter en funcionamiento. Imprime resultado en pantalla

--- src/cipher.js
+++ src/cipher.js
@@ -1,6 +1,12 @@
 window.cipher = {
-  encode: () => {
-    /* Acá va tu código */
+  encode: (string, offset) => {
+    let codedString = '';
+	for ( let i = 0; i < string.length; i++) {
+		let asciiCode = string.charCodeAt(i);
+		let asciiCodedString = (asciiCode - 65 + offset) % 26 + 65;
+		codedString += String.fromCharCode(asciiCodedString);
+	}
+	return codedString;
   },
   decode: () => {
     /* Acá va tu código */
--- src/index.js
+++ src/index.js
@@ -1,28 +1,15 @@
 // Introducing HTML elements to JavaScript
 const btnEncode = document.getElementById('btn-encode');
+const encodedMessage = document.getElementById('encoded-message');
 
-// TESTING const FOR, ALSO TESTING, FUCTION
-const stringUppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+// CONSTANTS
 const asciiCodeUppercase = 65;
 const totalStringUppercase = 26;
 
-// FUNCTION TO ENCODE
-// function encode () {
-    
-// }
-
-
-// ENCODE FUNCTION - FIRST STEP - TAKE INPUT VALUE
+//ENCODE FUNCTION - FIRST STEP - TAKE INPUT VALUE. ESTO ESTÁ FUNCIONANDO.
 btnEncode.addEventListener('click', () => {
-    let stringToEncode = document.getElementById('input-encode').value;
-    let encodeOffset = parseInt(document.getElementById('encode-offset').value);
-    let asciiCode = stringToEncode.charCodeAt(0);
-    let asciiCodedString = (asciiCode - asciiCodeUppercase + encodeOffset) % totalStringUppercase + asciiCodeUppercase;
-    let codedString = String.fromCharCode(asciiCodedString);
-    document.getElementById('encoded-message').innerHTML = codedString;
-    console.log(stringToEncode);
-    console.log(encodeOffset);
-    console.log(asciiCode);
-    console.log(asciiCodedString);
-    console.log(codedString);
-    })
+    let string = document.getElementById('input-encode').value;
+    let offset = parseInt(document.getElementById('encode-offset').value);
+    const codedString = cipher.encode(string, offset);
+    encodedMessage.innerHTML += codedString;
+})
